39789a5721ad4d3873a2665f395d9d4e82ecfc5c
meszdav <meszarosdavid91@gmail.com>
2020-08-29 18:13:00 +0200

    working model rfc_v14 with two classes

--- notebooks/processing_funcs.py
+++ notebooks/processing_funcs.py
@@ -11,61 +11,61 @@ import os
 
 
 def data_processing(df, sample_rate):
-    
+
     #creating time groups in order to evenly sample the time points
     df["time_groups"] = (df["time"] / sample_rate).apply(lambda x: int(x))
-    
+
     #dividing the time groups by 1/sample rate --> correct time stamps
     df = df.groupby(["time_groups"]).mean()
     df["time"] = df.index/(1/sample_rate)
-    
+
     measurementID = df["measurementID"]
-     
+
     time = df["time"]
-    
+
     gps_data = df[['lat', 'lon', 'height', 'velocity', 'direction', 'h_accuracy',
                    'v_accuracy']]
     gps_data = pd.concat([gps_data,
-                          pd.DataFrame(columns=["helper_1"], 
+                          pd.DataFrame(columns=["helper_1"],
                                        data = np.where(gps_data["lat"] >0,1,0))],
                          axis=1)
     gps_data["id_gps"] = gps_data["helper_1"].cumsum()
 
     gps_data.fillna(method="ffill", inplace = True)
     gps_data.drop("helper_1", axis = 1, inplace = True)
-    
-    
-    sensor_data = df[['x_lin_acc', 'y_lin_acc', 'z_lin_acc', 
-                     'x_gyro', 'y_gyro','z_gyro', 
+
+
+    sensor_data = df[['x_lin_acc', 'y_lin_acc', 'z_lin_acc',
+                     'x_gyro', 'y_gyro','z_gyro',
                      'x_acc', 'y_acc', 'z_acc']].interpolate(method='polynomial', order=2)
-    
+
     df = pd.concat([measurementID, time,gps_data,sensor_data], axis=1).dropna()
-    
+
     df["time_shift"] = df["time"].shift()
-    
+
     if round((df["time"] - df["time_shift"]).max(), 2) > sample_rate:
         pass
-    
+
     df.drop("time_shift", axis = 1, inplace = True)
-    
+
     return df
-    
-    
+
+
 def read_measurement(path, sample_rate):
 
     dff = pd.DataFrame()
 
     for p in glob.glob(path):
-        
+
         df = pd.DataFrame()
-        
+
         for c in glob.glob(os.path.join(p,"*") ):
-            
+
             new_df = pd.read_csv(c)
             new_df["sensor"] = c.split(".csv")[0].split('\\')[-1]
-            
 
-            df = pd.concat([new_df,df], axis=0)    
+
+            df = pd.concat([new_df,df], axis=0)
 
 
         df = df.pivot_table(index="Time (s)", columns="sensor")
@@ -81,35 +81,35 @@ def read_measurement(path, sample_rate):
 
         df.columns = [i for i in new_columns]
 
-        df =df[["time","lat", "lon", "height", 
+        df =df[["time","lat", "lon", "height",
                 "velocity", "direction",
-                "h_accuracy", "v_accuracy", 
-                "x_lin_acc", "y_lin_acc", "z_lin_acc", 
+                "h_accuracy", "v_accuracy",
+                "x_lin_acc", "y_lin_acc", "z_lin_acc",
                 "x_gyro", "y_gyro", "z_gyro",
                 "x_acc","y_acc","z_acc", ]]
 
         df["measurementID"] = int(c.split(".csv")[0].split('\\')[-2])
-     
-        df = data_processing(df, sample_rate)
-        
-        dff = pd.concat([df,dff])
-        dff = dff[dff["velocity"].abs()*3.6 > 20]  
 
-    
+        df = data_processing(df, sample_rate)
+
+        dff = pd.concat([df,dff])
+        dff = dff[dff["velocity"].abs()*3.6 > 20]
+
+
     return dff
-        
+
 
 def read_all(sample_rate = 0.02):
 
     q1 = read_measurement("../data/raw_data_train/rsq_q1/*",sample_rate=sample_rate)
     q1["label"] = 0
-    q2 = read_measurement("../data/raw_data_train/rsq_q2/*", sample_rate=sample_rate)
-    q2["label"] = 1
+    # q2 = read_measurement("../data/raw_data_train/rsq_q2/*", sample_rate=sample_rate)
+    # q2["label"] = 1
     q3 = read_measurement("../data/raw_data_train/rsq_q3/*", sample_rate=sample_rate)
-    q3["label"] = 2
+    q3["label"] = 1
 
 
-    df = pd.concat([q1,q2,q3],axis=0)
+    df = pd.concat([q1,q3],axis=0)
 
     cols = list(df.columns)
     cols.remove("time")
